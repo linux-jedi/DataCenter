53ec01e224d5c164da590b6a9173a31079a6c76c
linux-jedi <calebtho@usc.edu>
2017-01-21 17:54:14 -0800

    Enabled CORS for frontend connections

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from flask_cors import CORS, cross_origin
+
 import os
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 db = SQLAlchemy(app)
